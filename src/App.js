00ac702160e2e05c94bdb4d8fe0adaa3ed11230c
Harshal <gangurdeharsh08@gmail.com>
2020-07-26 13:47:44 +0530

    add burger builder component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <h1>Test</h1>
+        <BurgerBuilder></BurgerBuilder>
       </Layout>
     </div>
   );

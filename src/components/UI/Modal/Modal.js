131d08c0066504f970282fc5786c78c0d7e113ae
Harshal <gangurdeharsh08@gmail.com>
2020-08-02 18:25:24 +0530

    performance improvements

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.module.css';
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <div
-        className={classes.Modal}
-        style={{ transform: props.open ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.open ? 1 : 0 }}
-      >
-        {props.children}
-      </div>
-      <Backdrop show={props.open} close={props.close}></Backdrop>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.open !== this.props.open;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Aux>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.open ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.open ? 1 : 0
+          }}
+        >
+          {this.props.children}
+        </div>
+        <Backdrop show={this.props.open} close={this.props.close}></Backdrop>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
